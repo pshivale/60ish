fadee09d229e7a5442947302945f46ec6f494c1c
pshivale <pshivale@gmail.com>
2014-02-16 14:18:16 +0530

    factory not working

--- js/module.js
+++ js/module.js
@@ -17,13 +17,31 @@ demoApp.config(function($routeProvider){
 	.otherwise({redirectTo: '/'});
 });
 
-demoApp.controller('SimpleController', function($scope){
-	$scope.players = [
+demoApp.factory('simpleFactory', function(){
+	var players = [
 		{name: 'Ronaldo', club: 'Real Madrid', weekly: '1000'},
 		{name: 'Van parsie', club: 'Manchester United', weekly: '900'},
 		{name: 'Messi', club: 'Barcelona', weekly: '1010'},
 		{name: 'Silva', club: 'Manchester City', weekly: '850'}
 	];
+	var factory = {};
+	factory.getPlayers = function(){
+		return players;
+	};
+	factory.postPlayer = function(player){
+
+	};
+});
+
+demoApp.controller('SimpleController', [ '$scope', 'simpleFactory' , function($scope, simpleFactory){
+	$scope.players = [];
+	
+	init();
+
+	function init(){
+		players = simpleFactory.getPlayers();
+	}
+	
 	$scope.addPlayer = function(){
 		$scope.players.push(
 			{
@@ -32,4 +50,4 @@ demoApp.controller('SimpleController', function($scope){
 				weekly: $scope.newPlayer.weekly
 			});
 	}
-});
+}]);
